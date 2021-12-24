315ae5c70439480717d3e0c89c691e9196129f62
ISUMarco <96190501+ISUMarco@users.noreply.github.com>
2021-12-24 11:28:38 +0100

    Changed Code for my father
    Ralf halt

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello ISUMarco, but now a new commmit has been done!'));
+app.get('/', (req, res) => res.send('Hello ISUMarco, but now a new commmit has been done! Now for Ralf'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
